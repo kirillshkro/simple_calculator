9a4ff834190f91afb9ea560394fa0104f48ec732
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2024-01-18 08:21:47 +0300

    Little progress

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	var line string
-	var result int
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	line = scanner.Text()
-	go Calculate(line, result)
-	fmt.Printf("%s = %d\n", line, result)
+	var line chan string = make(chan string)
+	var quit chan int = make(chan int)
+	var input string
+	//defer close(line)
+	//defer close(quit)
+	fmt.Scan(&input)
+	go func() {
+		for {
+			if input == "exit" {
+				quit <- 0
+			}
+			calc(line, quit, input)
+		}
+	}()
+	fmt.Println(<-line)
+}
+
+func calc(text chan string, quit chan int, s string) {
+	for {
+		select {
+		case text <- s:
+			str := "Echo " + <-text
+			fmt.Println(str)
+		case <-quit:
+			fmt.Println("Quit")
+		}
+	}
 }
